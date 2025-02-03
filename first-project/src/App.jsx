0fb754a96be7deec397ad4ef1ae04431777e867b
Woladka <me@example.com>
2025-02-03 04:10:32 +0100

    updated

---
+++ first-project/src/App.jsx
@@ -0,0 +1,19 @@
+import Youtube from "./youtube"
+function App() {
+
+
+
+  return (
+    <>
+      <h1>Vite react app</h1>
+      <h1>Vite react app</h1>
+      <h2>Vite react app</h2>
+      <h2>Vite react app</h2>
+      <Youtube />
+
+
+    </>
+  )
+}
+
+export default App
